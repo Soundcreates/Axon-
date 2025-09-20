c0a984b142b6942731ede8c2d823eaf3c7a23806
Soundcreates <shantanav7@gmail.com>
2025-09-20 20:13:59 +0530

    Added deployment scripts and both contracts are deployed with their abi available in the client folder under contractData folder which is under src

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_API,
+      accounts:  [process.env.WALLET_PRIVATE_KEY]
+    }
+  }
 };
