d8716cc2f50e93d3f3966ea917a091a2d393d8c5
Soundcreates <shantanav7@gmail.com>
2025-10-12 11:56:45 +0530

    Integrating produrls

--- server/index.js
+++ server/index.js
@@ -5,23 +5,28 @@ import cors from "cors";
 import homeRouter from "./routes/indexRoutes.js";
 import connectDB from "./config/connectDB.js";
 
-
 dotenv.config();
 const app = express();
 
-app.use(express.urlencoded({extended:false}));  
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use (cors({
-    origin:[process.env.PROJECT_MODE==="development" ? "http://localhost:8080":process.env.PROD_URI],
+app.use(
+  cors({
+    origin: [
+      process.env.PROJECT_MODE === "development"
+        ? "http://localhost:8080"
+        : process.env.PROD_URI || "https://axon-dun.vercel.app/",
+    ],
     // methods:["GET","POST","DELETE","PUT","PATCH"],
-    credentials:true
-}));
+    credentials: true,
+  })
+);
 
-app.use("/api",homeRouter);
+app.use("/api", homeRouter);
 
-const PORT=process.env.PORT
-app.listen(PORT,()=> {
-     connectDB();
-    console.log("server started");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  connectDB();
+  console.log("server started");
 });
