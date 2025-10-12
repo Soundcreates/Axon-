73e2636d50347ab7b54ce8f05241c0d97c74ad63
Soundcreates <shantanav7@gmail.com>
2025-10-12 11:49:52 +0530

    integrating produrls pt2

--- client4/src/service/backendApi.ts
+++ client4/src/service/backendApi.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const server = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://axon-p64m.onrender.com/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL || "https://axon-p64m.onrender.com/api",
   withCredentials: true,
-})
+});
